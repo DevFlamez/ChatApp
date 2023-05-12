190148a48b15a13593ff71b69ac05286998a9276
DevFlamez <121361695+DevFlamez@users.noreply.github.com>
2023-05-12 15:13:48 +0100

    Delete vic.js
    deleted this

--- vic/vic.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#menu-btn');
-let navList = document.querySelector('.navList');
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navList.classList.toggle('open');
-
-}
